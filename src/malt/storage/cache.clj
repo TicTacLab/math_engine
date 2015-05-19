d551bc922448cf954cc37913e03dd8c4beefdc21
serzh <nechyporchuk@gmail.com>
2015-05-19 11:41:11 +0300

    fail silently on cache put/fetch
    Former-commit-id: eff65751d60a7f95010b18749bd6f5b8532d50b1

--- src/malt/storage/cache.clj
+++ src/malt/storage/cache.clj
@@ -2,21 +2,31 @@
   (:require [clojurewerkz.cassaforte.cql :as cql]
             [clojurewerkz.cassaforte.query :refer [where columns]]
             [taoensso.nippy :as nippy]
-            [metrics.meters :as meter])
-  (:import (com.datastax.driver.core.utils Bytes)))
+            [metrics.meters :as meter]
+            [clojure.tools.logging :as log])
+  (:import (com.datastax.driver.core.utils Bytes)
+           [com.datastax.driver.core.exceptions DriverException]))
 
 (defn put [{conn :conn} {model-id :id params :params} value]
-  (cql/insert conn "cache" {:model_id model-id
-                            :params   (nippy/freeze params)
-                            :result   (nippy/freeze value)}))
+  (try
+    (cql/insert conn "cache" {:model_id model-id
+                              :params   (nippy/freeze params)
+                              :result   (nippy/freeze value)})
+    (catch DriverException e
+      (log/error e "Exception during cache fetching")
+      nil)))
 
 (defn fetch [{conn :conn} {model-id :id params :params}]
-  (let [bs (:result (cql/get-one conn "cache"
-                                 (columns :result)
-                                 (where [[= :model_id model-id]
-                                         [= :params (nippy/freeze params)]])))]
-    (when bs
-      (nippy/thaw (Bytes/getArray bs)))))
+  (try
+    (let [bs (:result (cql/get-one conn "cache"
+                                   (columns :result)
+                                   (where [[= :model_id model-id]
+                                           [= :params (nippy/freeze params)]])))]
+      (when bs
+        (nippy/thaw (Bytes/getArray bs))))
+    (catch DriverException e
+      (log/error e "Exception during cache fetching")
+      nil)))
 
 (defmacro with-cache-by-key [storage key & body]
   `(if (:cache-on ~storage)
