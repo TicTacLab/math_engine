160de8d2ebe730c6a0b6319ce9f77383f89adf25
Eugene Linchenko <e.linchenko@favbet.com>
2015-07-22 14:07:17 +0300

    add initial reconnection to C*
    Former-commit-id: 550682e9fde0d78599275e6a5e4dc2cf399a3db7

--- src/malt/storage.clj
+++ src/malt/storage.clj
@@ -7,7 +7,21 @@
             [clojurewerkz.cassaforte.client :as cc]
             [clojurewerkz.cassaforte.policies :as cp]
             [clojure.tools.logging :as log])
-  (:import [com.codahale.metrics RatioGauge$Ratio]))
+  (:import [com.codahale.metrics RatioGauge$Ratio]
+           [com.datastax.driver.core.exceptions NoHostAvailableException]))
+
+
+(defn try-connect-times [times delay-ms nodes keyspace opts]
+  (let [result (try
+                 (cc/connect nodes keyspace opts)
+                 (catch NoHostAvailableException ex ex))]
+    (cond
+      (and (instance? Exception result) (zero? times)) (throw result)
+      (instance? Exception result) (do
+                                     (log/warnf "Failed to connect to Cassandra, will retry after %d ms" delay-ms)
+                                     (Thread/sleep delay-ms)
+                                     (recur (dec times) delay-ms nodes keyspace opts))
+      :else result)))
 
 (defrecord Storage [conn
                     storage-nodes
@@ -21,11 +35,13 @@
   component/Lifecycle
 
   (start [component]
-    (let [conn (cc/connect storage-nodes
-                           storage-keyspace
-                           {:credentials {:username storage-user
-                                          :password storage-password}
-                            :reconnection-policy (cp/constant-reconnection-policy 100)})
+    (let [conn (try-connect-times 1000
+                                  1000
+                                  storage-nodes
+                                  storage-keyspace
+                                  {:credentials         {:username storage-user
+                                                         :password storage-password}
+                                   :reconnection-policy (cp/constant-reconnection-policy 100)})
           hits (meter/meter "hits")
           calls (meter/meter "calls")]
       (log/info "Storage started")
