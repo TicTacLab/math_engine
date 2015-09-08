bb60ae6ef59b291a77eeb12422d5cbee6b265380
serzh <nechyporchuk@gmail.com>
2015-09-08 10:56:17 +0300

    remove boot
    Former-commit-id: 811c014ef767a515376c5a4186f3a14751cc3e06

--- boot.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export BOOT_JVM_OPTIONS="$BOOT_JVM_OPTIONS -Xmx4g -XX:MaxPermSize=128m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -client -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none"
-boot "$@"
